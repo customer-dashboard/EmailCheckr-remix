5819412c25088a6edfb4a52a99454f52a14d2d2b
customer-dashboard <customerdashboardpro@gmail.com>
2025-04-04 11:00:27 +0530

    04-04-25

--- app/db.server.js
+++ app/db.server.js
@@ -1,6 +1,13 @@
+import { InsertUpdateData } from "./server/mongodb";
 
 export const UninstallEvent = (session)=>{
   console.log("app has uninstalled", session);
   
+      const resData = {
+        status: 0,
+      }
+      const result = InsertUpdateData(session.shop, resData, 'shop_info');
+      console.log("result", result);
+      return resData;
 }
 
